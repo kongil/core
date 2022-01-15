3e77ca26ec62b49608afaac83c5d51bd86e4313a
o_o01 <o_o0122@naver.com>
2022-01-15 23:21:17 +0900

    1. 스프링 컨테이너와 스프링 빈 2. 싱글톤 컨테이너 3. 컴포넌트 스캔

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContext {
+public class ApplicationContextInfoTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
@@ -21,7 +21,7 @@ public class ApplicationContext {
     }
 
     @Test
-    @DisplayName("모든 빈 출력하기")
+    @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
